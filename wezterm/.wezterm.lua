3fe7fe4bfcff8ac20ee2cc5ef6ae9503d9ae6e37
Henrique Bacelar <dev@bacelar.fr>
2024-08-31 15:47:43 +0200

    Make wezterm window transparent

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the Wezterm API
-local wezterm = require "wezterm"
+local wezterm = require("wezterm")
 
 -- This will hold the configuration
 local config = wezterm.config_builder()
@@ -9,16 +9,17 @@ config.color_scheme = "Catppuccin Mocha"
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.9
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 10.0
 config.enable_wayland = false
 
 config.keys = {
-  {
-    key = "Z",
-    mods = "CTRL",
-    action = wezterm.action.TogglePaneZoomState,
-  }
+	{
+		key = "Z",
+		mods = "CTRL",
+		action = wezterm.action.TogglePaneZoomState,
+	},
 }
 
 -- Return the configuration to Wezterm
