e9f4719a28e3668e7e49d63a9307a5aca5f54a30
Henrique Bacelar <dev@bacelar.fr>
2024-09-01 19:04:10 +0200

    Update neovim config

---
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -0,0 +1,71 @@
+local configs = require "nvchad.configs.lspconfig"
+local map = vim.keymap.set
+
+local servers = {
+  clangd = {
+    on_attach = function(client, bufnr)
+      client.server_capabilities.signatureHelpProvider = false
+
+      local function opts(desc)
+        return { buffer = bufnr, desc = "LSP " .. desc }
+      end
+
+      -- Custom keymap
+      map("n", "<leader>sch", "<cmd>ClangdSwitchSourceHeader<cr>", opts "Switch Source/Header (C/C++)")
+    end,
+    settings = {
+      clangd = {
+        InlayHints = {
+          Designators = true,
+          Enabled = true,
+          ParameterNames = true,
+          DeducedTypes = true,
+        },
+        fallbackFlags = { "-std=c++20" },
+      },
+    },
+    root_dir = function(fname)
+      return require("lspconfig.util").root_pattern(
+        "Makefile",
+        "configure.ac",
+        "configure.in",
+        "config.h.in",
+        "meson.build",
+        "meson_options.txt",
+        "build.ninja"
+      )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(fname) or require(
+        "lspconfig.util"
+      ).find_git_ancestor(fname)
+    end,
+    cmd = {
+      "clangd",
+      "--background-index",
+      "--clang-tidy",
+      "--header-insertion=iwyu",
+      "--completion-style=detailed",
+      "--function-arg-placeholders",
+      "--fallback-style=llvm",
+    },
+    init_options = {
+      usePlaceholders = true,
+      completeUnimported = true,
+      clangdFileStatus = true,
+    },
+  },
+}
+
+for name, opts in pairs(servers) do
+  opts.on_init = configs.on_init
+  opts.capabilities = configs.capabilities
+
+  require("lspconfig")[name].setup {
+    on_init = opts.on_init,
+    capabilities = opts.capabilities,
+    settings = opts.settings,
+
+    on_attach = function(client, bufnr)
+      opts.on_attach(client, bufnr)
+      configs.on_attach(client, bufnr)
+    end,
+  }
+end
---
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -0,0 +1,69 @@
+return {
+  -- Conform
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre", -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- LSP Config
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = true },
+    },
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  -- Inlay Hints
+  {
+    "MysticalDevil/inlay-hints.nvim",
+    event = "LspAttach",
+    -- config = function()
+    --   require("inlay-hints").setup()
+    -- end,
+  },
+
+  -- Rustaceanvim
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^5",
+    lazy = false,
+    config = function()
+      require "configs.rustaceanvim"
+    end,
+  },
+
+  -- Treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      auto_install = true,
+      ensure_installed = {
+        "c",
+        "cpp",
+        "cmake",
+        "rust",
+      },
+    },
+  },
+
+  -- Lazygit
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
+}
