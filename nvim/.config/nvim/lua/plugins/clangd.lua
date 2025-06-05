ef4b00b815ad0f620b8c3ae9ddb9bd48f5830049
H BACELAR <dev@bacelar.fr>
2025-06-05 11:54:45 +0200

    Update btop, nvim and yazi

---
+++ nvim/.config/nvim/lua/plugins/clangd.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        clangd = {
+          cmd = {
+            "clangd",
+            "--query-driver=/usr/bin/arm-none-eabi-g++",
+            "--background-index",
+            "--clang-tidy",
+            "--header-insertion=iwyu",
+            "--completion-style=detailed",
+            "--function-arg-placeholders",
+            "--fallback-style=llvm",
+          },
+        },
+      },
+    },
+  },
+}
