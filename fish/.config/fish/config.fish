56d844f0f402a16201ec5afc37e3b5ef5e2b3564
H Bacelar <dev@bacelar.fr>
2025-10-26 20:46:50 +0100

    [fish] Add Rosé Pine theme

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,4 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Configure theme (Rosé Pine)
+fish_config theme choose rose-pine
+
+# Init starship
 starship init fish | source
